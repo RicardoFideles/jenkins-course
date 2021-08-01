8350b37c08d18e305adc0d385a1c443f10d16043
Ricardo <ricardo@ricardofideles.com.br>
2021-08-01 17:54:21 -0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('RicardoFideles/docker-nodejs-demo')
+            repositoryName('ricardofideles/docker-nodesjs-devops')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
